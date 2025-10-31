30883bc9b194e9779c7da3a11c029cd89fa8a42a
Pieter van Ginkel <pvginkel@gmail.com>
2025-10-31 21:42:40 +0100

    Fixed vite docs configuration.

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,8 @@
+export default {
+  srcExclude: ['features/**', 'epics/**'],
+  markdown: {
+    // Disables raw HTML in markdown entirely. It works badly with the template
+    // syntax used in the command files.
+    html: false
+  }
+}
