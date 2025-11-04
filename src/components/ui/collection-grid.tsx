e70e94dc5b267ee2948b2f1e969c3d53612a4452
Pieter van Ginkel <pvginkel@gmail.com>
2025-11-04 20:59:32 +0100

    Show four columns on grids on large screens.

--- src/components/ui/collection-grid.tsx
+++ src/components/ui/collection-grid.tsx
@@ -5,7 +5,7 @@ export interface CollectionGridProps {
 
 export function CollectionGrid({ children, testId }: CollectionGridProps) {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3" data-testid={testId}>
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4" data-testid={testId}>
       {children}
     </div>
   );
